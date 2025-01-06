2b597c19b82b079d2eecaf7adf7bbe4b1a71c4ae
mosabami <aayodeji@microsoft.com>
2025-01-06 06:49:09 -0800

    add gh pages config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/walchat/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
